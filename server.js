3f6e805b87935fa98bec82915388ee9e00ac4abe
Jonathan M <carljonathansebastian@gmail.com>
2013-06-10 22:07:33 +0200

    La till mer funktioner

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var app = express();
 
 app.get('/drinks', drinks.getAllDrinks);
 app.get('/drinks/:id', drinks.getDrinkById);
+app.get('/drinks/ingredients/:id', drinks.getIngredientsByDrinkId);
+
+app.get('/ingredients', drinks.getAllIngredients);
+app.get('/ingredients/:id', drinks.getIngredientById);
  
+app.post('/drinks', drinks.getDrinkByIngredient);
+
 app.listen(3000);
 console.log('Listening on port 3000...');
